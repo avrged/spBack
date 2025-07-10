06104ba0d7528d06182a989160756fa1a0cdfee6
avrged <edmontoyanobl@gmail.com>
2025-07-10 00:29:07 -0600

    mkII

--- src/main/java/org/sazonpt/di/AppModule.java
+++ src/main/java/org/sazonpt/di/AppModule.java
@@ -1,10 +1,5 @@
 package org.sazonpt.di;
 
 public class AppModule {
-    //repositorio que controla todo
 
-    //crear administrador
-    //buscar administrador por id
-    //actualizar administrador
-    //eliminar administrador
 }
